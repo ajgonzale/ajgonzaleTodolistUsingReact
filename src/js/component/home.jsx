ba1be85f8e7f80766b4235b7ad6c89fec9277da5
Alejandro Gonzalez <ajgonzale@gmail.com>
2024-07-15 18:49:09 +0000

    add a todo list

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,24 +1,44 @@
-import React from "react";
-
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
+import React, { useState } from "react";
 
 //create your first component
 const Home = () => {
+
+	const [list, setList] = useState(['Make the bed', 'Make a bot', 'develop an project']);
+	const [item, setItem] = useState('');
+
+	function addItem() {
+		console.log(list);
+		setList([...list, item]);
+		setItem('');		
+	}
+
+	function removeItem(index) {
+		let aux = list;
+		aux[index] = null;
+		aux = aux.filter(n => {return n != null})
+		setList(aux);
+	}
+
 	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+		<div className="text-center container-fluid d-flex justify-content-center">
+			<div>
+				<h1 className="text-center mt-5 tittle" >todos</h1>
+				<div>
+					<input type="text" className="w-100 border-bottom input" onChange={e => setItem(e.target.value)} onKeyDown={(e) => {if (e.key === 'Enter') addItem()}} value={item}></input>
+				</div>
+				<div id='parentItems'>{ 
+							list.map((item, index) => {
+								return <div key={index} className="itemList"><div>{item}</div><i className="fas fa-times" onClick={e => removeItem(index)}></i></div>
+							}) 
+						}
+				</div>
+				<div className='bottomList'>{list.length} item left</div>
+				<p className="p-3">
+					Made by{" "}
+					<a href="#">Alejandro Gonzalez</a>, with
+					love!
+				</p>`
+			</div>
 		</div>
 	);
 };
